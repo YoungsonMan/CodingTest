623aecf44d3867ddd0c6fd215e788c34e71604ba
YoungsonMan <choi13570@naver.com>
2024-08-07 23:54:32 +0900

    [Save] Save Day3
    saving point

---
+++ 240806_01-05_Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _240806_01_05_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
