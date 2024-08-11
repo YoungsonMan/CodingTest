c8c71a7dc8950d87dea381f3903c5fa8b74703f8
YoungsonMan <choi13570@naver.com>
2024-08-11 14:49:45 +0900

    [Add] finish Day 4
    Solved 2 more problems from day 4 and day 4 done.

--- 240810_04_ConditionalString/Program.cs
+++ 240810_04_ConditionalString/Program.cs
@@ -39,7 +39,7 @@
             n = 20;
             m = 50;
             Solution result = new Solution();
-            result.solution(ineq,eq,n,m);
+            result.solution(ineq, eq, n, m);
             Console.WriteLine(result.solution(ineq, eq, n, m));
         }
 
@@ -52,44 +52,95 @@
                 // 어차피 무조건 m 이더 크다.
                 // 논리적으로 n이나 m은 뭐 필요 없다.
                 // eq( = , ! )도 뭐 딱히 상관 없고 그냥 
-                
+
                 // 음.. 테스트에서 다른 변수들도 나오는구나
                 // n & m 둘다 고려해야한다.
 
                 // int n,m을 그냥 string으로 바꿔서 비교해서 같으면 1 아니면 0되게??
+                // .Equals
                 int answer = 0;
 
-                string string1, string2, comb1, comb2;
+                if (ineq == "<" && eq == "=")
+                {
+                    if (n <= m) { answer++; }
+                }
+                else if (ineq == ">" && eq == "=")
+                {
+                    if (n >= m) { answer++; }
+                }
+                else if (ineq == "<" && eq == "!")
+                {
+                    if (n < m) { answer++; }
+                }
+                else if (ineq == ">" && eq == "!")
+                {
+                    if (n > m) { answer++; }
+                }
 
-                string1 = $"{n}{ineq}{eq}{m}";
+                return answer;
 
-                Console.WriteLine(string1);
+
+                /// 몇시간동안 박으면서 
+                /// 이게 ! , = 에서 !이 참이 아닐때 쓰는데 왜 이게 있으면 반대로 받아야하는거 아닌가
+                /// 이러한 생각 때문에 더 했갈렸다
+                /// ex) >! 이러면 오른쪽이 더 커야하는거 아닌가 라던가
+                /// 잘 안되다보니 이게 별의별 이상한걸 다 시도해보려했던거같다
+                /// 로직은 이해는 했었는데 구현을 어떻게 해야하는가 쪽에서 막혔었다.
+                /// 통채로 string1 = $"{n}{ineq}{eq}{m}"; 이런식으로 문자열로 만들어서
+                /// 이 문자열이 참이면 1, 아니면 0 이런식으로 하려고했는데
+                /// 이게 또 막상하려다보니까 뭔가 생각했던 것보다 더 복잡했다.
+                /// 이게 뭔가 logical 하고 simple하게 사고하는 방식이 더 필요한거 같다.
+                
+                /// 다른사람들보면 ? 물음표를 많이 쓰는데 찾아봐도 뭔가가 뭔가 햇갈린다.
+                /// 그래서 안쓰고 그냥 ==로 했는데 뭔가 봐도봐도 이해가 안되는? 햇갈리는 것들이 있다.
+
+
+                //int answer = 0;
+                //
+                //bool isTrue = true;
+                //
+                //string string1, string2, comb1, comb2;
+                //string compare1, compare2, compare3, compare4;
+                //compare1 = 
+                //string1 = $"{n}{ineq}{eq}{m}";
+                //
+                //switch (string1)
+                //{
+                //    case string s:
+                //        answer ++;
+                //        break;
+                //}
+                //
+
                 //string1 = n.ToString();
                 //string2 = m.ToString();
                 //
                 //comb1 = string1+ineq+string2;
                 //Console.WriteLine(comb1);
 
+                /// 밑에 여기서 if를 한번 더 써서 구축을 해봐야 한단거를
+                /// 생각을 못했었다.
 
-                if (n < m)
-                {
-                    answer = 1;
-                }
-                else if (n > m)
-                {
 
-                }
+                //if (n < m)
+                //{
+                //    answer = 1;
+                //}
+                //else if (n > m)
+                //{
+                //
+                //}
+                //
+                //if (ineq == "<")
+                //{
+                //
+                //}
+                //else if (ineq == ">")
+                //{
+                //    answer = 0;
+                //}
 
-                if (ineq == "<")
-                {
 
-                }
-                else if (ineq == ">")
-                {
-                    answer = 0;
-                }
-                
-                return answer;
             }
         }
     }
---
+++ 240811_05_ConditionalFlag/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _240811_05_ConditionalFlag
+{   /// <summary>
+    /// 2024-08-11
+    /// 프로그래머스 :
+    /// 코딩테스트 연습 > 코딩 기초 트레이닝 > Day 4 연산, 조건문 
+    /// URL: https://school.programmers.co.kr/learn/courses/30/lessons/181933
+    /// </summary>
+    public class Program
+    {/****************************************************************
+     * 공배수
+     * 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+     * flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+     * 
+     * 제한사항:
+     *        -1,000 ≤ a, b ≤ 1,000
+     *       
+     * 입출력 예)
+     *     a     |     b     |	  flag    |   result
+     *    -4     |     7     |	  true    |	    3
+     *    -4     |     7     |	 false    |	   -11
+     *****************************************************************/
+        static void Main(string[] args)
+        {
+            Solution[] result = new Solution[4];
+            result[0] = new Solution();
+            result[1] = new Solution();
+            result[2] = new Solution();
+            result[3] = new Solution();
+
+            //result[0].solution(-4, 7, true);
+            //result[1].solution(-4, 7, false);
+            //result[2].solution(-12, -12, true);
+            //result[3].solution(-12, -12, false);
+            Console.WriteLine(result[0].solution(-4, 7, true));
+            Console.WriteLine(result[1].solution(-4, 7, false));
+            Console.WriteLine(result[2].solution(-12, -12, true));
+            Console.WriteLine(result[3].solution(-12, -12, false));
+        }
+        public class Solution
+        {
+            public int solution(int a, int b, bool flag)
+            {
+                int answer = 0;
+                if (flag.Equals(true))
+                {
+                    answer = a + b;
+                }
+                else if (flag.Equals(false))
+                {
+                    answer = a - b;
+                }
+
+                return answer;
+            }
+        }
+        /// 이전 문제에서 .Equals를 검색해본게 여기서 도움이 됐다.
+        /// 아 근데 맨날 이거 테스트 돌리는데 위에 프로그램 안바꿔가지고  <summary>
+        /// 이전 문제에서 .Equals를 검색해본게 여기서 도움이 됐다.
+        /// 다른 프로그램 돌아가서 이상하게 돌아가서 햇깔렸다. 
+    }
+}
